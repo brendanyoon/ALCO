ecabeeb8531b5ba0d0bff5f7b22567adb3c829e1
candykuo <candikuo@yahoo.com>
2022-04-14 17:32:09 -0400

    added .gitignore to solve pycache merge issues

--- app/urls.py
+++ app/urls.py
@@ -2,12 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-<<<<<<< HEAD
-    path('', views.home, name='app-home')
-]
-=======
     path('', views.home, name='app-home'),
     path('prof-dashboard/', views.prof_dashboard, name='prof-dash')
 ]
 
->>>>>>> 29d1dc4a964e09f7d8ffb3c0580bd3aebb88de35
