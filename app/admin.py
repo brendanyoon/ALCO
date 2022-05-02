73e4d86d7f020c0e12d4c4c19c2650338e427ca0
ikuo3 <99050445+ikuo3@users.noreply.github.com>
2022-05-02 16:14:21 -0400

    Revert "Dev sprint 3 experience"

--- app/admin.py
+++ app/admin.py
@@ -1,6 +1,3 @@
 from django.contrib import admin
-from .models import Professor, Student
 
-admin.site.register(Professor)
-admin.site.register(Student)
-
+# Register your models here.
--- app/views.py
+++ app/views.py
@@ -5,8 +5,7 @@
 #from rest_framework import Response
 
 from django.shortcuts import render
-from django.core.files.storage import FileSystemStorage
-from app.exp import exp
+from django.http import HttpResponse
 
 def home(request):
     return render(request, 'app/home.html')
@@ -14,32 +13,5 @@ def home(request):
 def prof_dashboard(request):
     return render(request, 'app/prof-dashboard.html')
 
-def prof_quizzes(request):
-    context = {}
-    if request.method == 'POST':
-        uploaded_file = request.FILES['document']
-        fs = FileSystemStorage()
-        name = fs.save(uploaded_file.name, uploaded_file)
-        context['url']= fs.url(name)
-
-    return render(request, 'app/prof-quizzes.html', context)
-
-def prof_map(request):
-    return render(request, 'app/prof-map.html')
-
 def student_dashboard(request):
-    return render(request, 'app/student-dashboard.html')
-
-def student_stats(request):
-    xp = 125999 #Here, we would get exp from the database. Placeholder number for now
-    level = exp.GetLevel(xp)
-    percent = round(exp.ToNextLevelPercent(xp) * 100, 2)
-
-    context = {
-        'xp': xp,
-        'level': level,
-        'percent': str(percent)+"%",
-        'ariapercent': str(percent)
-    }
-
-    return render(request, 'app/student-stats.html', context=context)
+   return render(request, 'app/student-dashboard.html')
