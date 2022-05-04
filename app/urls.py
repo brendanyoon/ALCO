9befda32c8e59388247900b30d783bc7f00e1583
candykuo <candikuo@yahoo.com>
2022-05-04 13:46:11 -0400

    Updated nav bar for both student and professor dashboards to be the same style. Refactored code so that now both the prof and student dashboards have a base html and other views will now be extended from the files that have the respective nav bars. Updated colors for student and professor. Added in a carousel view for both student and professor for quiz view.

--- app/urls.py
+++ app/urls.py
@@ -7,6 +7,9 @@ urlpatterns = [
     path('student-dashboard/', views.student_dashboard, name='student-dash'),
     path('prof-quizzes/', views.prof_quizzes, name='prof-quizzes'),
     path('prof-map/', views.prof_map, name='prof-map'),
-    path('student-stats/', views.student_stats, name='student-stats')
+    path('prof-quest/', views.prof_quest, name='prof-quest'),
+    path('student-stats/', views.student_stats, name='student-stats'),
+    path('student-map/', views.student_map, name='student-map'),
+    path('student-quest/', views.student_quest, name='student-quest')
 ]
 
