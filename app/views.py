ecf25a038577dc69a6cbd7c3c22258fae149aa5b
TubaPope <ja51650@umbc.edu>
2022-05-12 19:06:25 -0400

    Revert "Implemented database functionality to render exp based on account. Anything further requires more database development / quiz functionality."
    This reverts commit 414eaec8f6216ccf7664ce9a03f65511e9e249fa.

--- app/views.py
+++ app/views.py
@@ -9,8 +9,6 @@ from django.core.files.storage import FileSystemStorage
 from app.exp import exp
 from .models import Student, Completed_Quest
 
-global username
-
 def home(request):
     return render(request, 'app/home.html')
 
@@ -31,24 +29,17 @@ def prof_map(request):
     return render(request, 'app/prof-map.html')
 
 def student_dashboard(request):
-    global username
-    username = 'xyz@umbc.edu'
-    student = Student.objects.get(student_email = username)
-    name = student.first_name + ' ' + student.last_name
-    return render(request, 'app/student-dashboard.html', context = {'name': name, 'level': exp.GetLevel(student.exp_pts)})
+    student = Student.objects.get(student_email='xyz@umbc.edu')
+    name = student.first_name
+    return render(request, 'app/student-dashboard.html', context = {'name': name, 'level': exp.GetLevel(xp)})
 
 def student_map(request):
-    student = Student.objects.get(student_email=username)
-    name = student.first_name + ' ' + student.last_name
-    xp = student.exp_pts
-    return render(request, 'app/student-map.html', context = {'level': exp.GetLevel(xp), 'name': name})
+    xp = 125999
+    return render(request, 'app/student-map.html', context = {'level': exp.GetLevel(xp)})
 
 def student_quest(request):
-    student = Student.objects.get(student_email=username)
-    name = student.first_name + " " + student.last_name
-    xp = student.exp_pts
-    context = { 'level': exp.GetLevel(xp),
-                'name': name }
+    xp = Student.exp()
+    context = { 'level': exp.GetLevel(xp) }
     return render(request, 'app/student-quest.html', context = context)
 
 def prof_quest(request):
@@ -59,10 +50,8 @@ def student_stats(request):
     xp = student.exp_pts
     level = exp.GetLevel(xp)
     percent = round(exp.ToNextLevelPercent(xp) * 100, 2)
-    name = student.first_name + " " + student.last_name
 
     context = {
-        'name': name,
         'xp': xp,
         'level': level,
         'percent': str(percent)+"%",
